c8f68596e88f8b987261fd06bd3ca8e4763263b6
Timothy Coleman <timhc22@gmail.com>
2021-03-21 07:57:33 +0000

    Checkpoint

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    RPC_URL_1: 'https://mainnet.infura.io/v3/bea1015796574f66967247dfbcefb9dc',
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector';
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector';
+// import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
+import { Web3Provider } from '@ethersproject/providers';
+
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
     <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
