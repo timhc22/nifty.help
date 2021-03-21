91579008f7b39ff3da25164204c0956c142f5d64
Timothy Coleman <timhc22@gmail.com>
2021-03-21 05:24:45 +0000

    Add 3d stuff

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,9 +1,32 @@
-import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import * as THREE from "three";
+import ReactDOM from 'react-dom'
+import React, { Suspense, useState, useRef, useEffect, useMemo } from 'react'
+import { Canvas, extend, useLoader, useThree, useFrame } from 'react-three-fiber'
+
+
+function Box({url}: any) {
+  const [texture]: any = useLoader(THREE.TextureLoader, url);
+  const mesh = useRef()
+  const [hover, setHover] = useState(false)
+
+  return (
+    <mesh
+      ref={mesh}
+      position={[0, 0, 0]}
+      scale={hover ? [1.2, 1, 1.2] : [1, 1, 1]}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+
+      <boxBufferGeometry attach="geometry" args={[1,1,1]} />
+      <meshStandardMaterial attach="material" map={texture} transparent={true} />
+    </mesh>
+
+  )
+}
 
 export default function DashboardPage(): JSX.Element {
-
   const [imageUrl, setImageUrl] = useState("");
   const apiURL = "https://gc4fvpbabl.execute-api.eu-west-2.amazonaws.com/live";
 
@@ -29,8 +52,12 @@ export default function DashboardPage(): JSX.Element {
           </Link>
         </section>
 
-
-        <img src={imageUrl} />
+        <Canvas camera={{ position: [2,2,2]}}>
+          <Suspense fallback={null}>
+            <Box url={[imageUrl]}/>
+          </Suspense>
+          <ambientLight />
+        </Canvas>
 
       </div>
     </section>
