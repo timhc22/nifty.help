3352a6816bc70e836acb8fdf23b0c15d074f2b9d
Timothy Coleman <timhc22@gmail.com>
2021-03-21 15:34:10 +0000

    Add styling to last page

--- src/data/pageTextWhat.js
+++ src/data/pageTextWhat.js
@@ -1,9 +1,9 @@
 const data = [
   { id: 1, heading: "A Really Short Story With Not Many Pages", text: "Some things change the world. Nfts (or niftys) are definitely one of them. They are what those bitcoin and blockchain people have been trying to tell you about for ages that they couldn't really explain very well (you thought it was you not getting it didn't you).\n" },
-  { id: 2, heading: "", text: "Yes, so nfts are pretty much the same as bitcoins (yes I can already hear the techies yelling, don't worry, I know not exactly). The only thing that is different about NFTs is that they are non-fungible. Uhh what? Fungi-what? Mushrooms?\n" },
-  { id: 3, heading: "", text: "Fungible just means something you can swap for something else without losing anything (think of swapping a 1p coin for another 1p coin). Non-fungible.. The opposite.. Would you swap a sketch on a piece of paper for the Mona lisa? Maybe if you could.. But there is something different about them, even though they are both drawings of stuff on some material.\n" },
-  { id: 4, heading: "", text: "So fungible is the 'F' in NFT, and.. You guessed it.. N is the 'Non'. So what is the T? T is Token.  A token like.. something representing something else, like.. A physical soccer ticket which grants me the ability to go to a specific game at a specific time? Yes that's it! Something representing something else.. Only digital..\n" },
-  { id: 5, heading: "", text: "Is he really using comic sans for this site..?? 🤔\n" },
+  { id: 2, heading: "", text: "Yea, so NFTs are pretty much the same as Bitcoins (yes I can already hear the techies yelling, don't worry, I know they are not exactly). The only thing that is different about NFTs is that they are non-fungible. Uhh what? Fungi-what? Mushrooms?\n" },
+  { id: 3, heading: "", text: "Fungible just means something you can swap for something else without losing anything (think of swapping a 1p coin for another 1p coin). Non-fungible.. The opposite.. Would you swap a sketch on a piece of paper for the Mona lisa? Maybe if you could.. But there is something different about them, even though they are both drawings of stuff on material.\n" },
+  { id: 4, heading: "", text: "So fungible is the 'F' in NFT, and.. You guessed it.. N is the 'Non'. So what is the T? T is Token.  A token like.. something representing something else, like.. A physical soccer ticket which grants you the ability to go to a specific game at a specific time? Yes that's it! Something representing something else.. Only digital..\n" },
+  { id: 5, heading: "", text: "And thats it in a nutshell. Click the link below to check out why we need them.. (Is he really using comic sans for this site..?? 🤔)\n" },
 ];
 
 export { data };
--- src/pages/PageFive.tsx
+++ src/pages/PageFive.tsx
@@ -6,8 +6,11 @@ export default function PageFive(): JSX.Element {
   return (
     <section className="container">
       <div>
+        <Typography paragraph className="pageText--heading">
+          Questions? Yea me too, plenty!
+        </Typography>
         <Typography paragraph>
-          Questions? Yea me too, plenty! <br/>I'll add some faqs on here when the hackathon is over (especially about NFTs and Climate change). <br/>Right now, I have 30 mins to get everything in, so catch you later!
+          <br/>I'll add some faqs on here when the hackathon is over (especially about NFTs and Climate change). <br/>Right now, I have 30 mins to get everything in, so catch you later!
         </Typography>
       </div>
     </section>
